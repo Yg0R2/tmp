180972b5ba68eb25fcbcddc52b99f44022a8a371
Tibor Kovacs <v-tikovacs@hotels.com>
2019-02-12 16:52:51 +0100

    Add tmp skeleton

---
+++ src/main/java/com/yg0r2/tmp/TmpApplication.java
@@ -0,0 +1,14 @@
+package com.yg0r2.tmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmpApplication.class, args);
+	}
+
+}
+
